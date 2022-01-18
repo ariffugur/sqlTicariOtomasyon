a535c6b88ecedb97546d476407043f350fab848e
sehalperen <seha.alperen@gmail.com>
2022-01-18 15:12:08 +0300

    Seçili fatura ürünlerinin listeleme

--- ticari_otomasyon/FrmFaturaDetay.cs
+++ ticari_otomasyon/FrmFaturaDetay.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace ticari_otomasyon
 {
@@ -18,9 +19,18 @@ namespace ticari_otomasyon
         }
 
         public string id;
+        sqlBaglantisi bgl = new sqlBaglantisi();
+
+        void listele()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Select * from Tbl_FaturaDetay where FaturaID='"+id+"'",bgl.baglanti());
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            gridControl1.DataSource = dt;
+        }
         private void FrmFaturaDetay_Load(object sender, EventArgs e)
         {
-            label1.Text = id;
+            listele();
         }
     }
 }
