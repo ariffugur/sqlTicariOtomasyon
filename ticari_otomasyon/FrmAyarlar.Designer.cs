a4ce945b64fe18886a5c6ae0b97eca7d52bd321e
sehalperen <seha.alperen@gmail.com>
2022-01-21 02:13:42 +0300

    Yeni Kullanıcı Tanımlama

--- ticari_otomasyon/FrmAyarlar.Designer.cs
+++ ticari_otomasyon/FrmAyarlar.Designer.cs
@@ -31,15 +31,17 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmAyarlar));
             this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.gridControl1 = new DevExpress.XtraGrid.GridControl();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Btnİslem = new System.Windows.Forms.Button();
             this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
             this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TxtPass = new System.Windows.Forms.TextBox();
+            this.TxtKulAd = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // gridView1
@@ -56,26 +58,27 @@
             this.gridControl1.Location = new System.Drawing.Point(0, 0);
             this.gridControl1.MainView = this.gridView1;
             this.gridControl1.Name = "gridControl1";
-            this.gridControl1.Size = new System.Drawing.Size(804, 225);
+            this.gridControl1.Size = new System.Drawing.Size(669, 165);
             this.gridControl1.TabIndex = 11;
             this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
             // 
-            // button1
+            // Btnİslem
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button1.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(58, 404);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 32);
-            this.button1.TabIndex = 18;
-            this.button1.Text = "KAYDET";
-            this.button1.UseVisualStyleBackColor = false;
+            this.Btnİslem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Btnİslem.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Btnİslem.Location = new System.Drawing.Point(78, 342);
+            this.Btnİslem.Name = "Btnİslem";
+            this.Btnİslem.Size = new System.Drawing.Size(110, 32);
+            this.Btnİslem.TabIndex = 18;
+            this.Btnİslem.Text = "KAYDET";
+            this.Btnİslem.UseVisualStyleBackColor = false;
+            this.Btnİslem.Click += new System.EventHandler(this.Btnİslem_Click);
             // 
             // simpleButton3
             // 
             this.simpleButton3.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton3.ImageOptions.Image")));
-            this.simpleButton3.Location = new System.Drawing.Point(11, 334);
+            this.simpleButton3.Location = new System.Drawing.Point(31, 272);
             this.simpleButton3.Name = "simpleButton3";
             this.simpleButton3.Size = new System.Drawing.Size(22, 23);
             this.simpleButton3.TabIndex = 17;
@@ -86,7 +89,7 @@
             this.simpleButton2.Appearance.Options.UseForeColor = true;
             this.simpleButton2.Cursor = System.Windows.Forms.Cursors.No;
             this.simpleButton2.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton2.ImageOptions.Image")));
-            this.simpleButton2.Location = new System.Drawing.Point(11, 261);
+            this.simpleButton2.Location = new System.Drawing.Point(31, 199);
             this.simpleButton2.Name = "simpleButton2";
             this.simpleButton2.Size = new System.Drawing.Size(22, 23);
             this.simpleButton2.TabIndex = 16;
@@ -96,51 +99,62 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label3.ForeColor = System.Drawing.Color.PaleGoldenrod;
-            this.label3.Location = new System.Drawing.Point(39, 335);
+            this.label3.Location = new System.Drawing.Point(59, 273);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(51, 18);
             this.label3.TabIndex = 15;
             this.label3.Text = "Şifre:";
             // 
-            // textBox2
+            // TxtPass
             // 
-            this.textBox2.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textBox2.Location = new System.Drawing.Point(11, 363);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(211, 25);
-            this.textBox2.TabIndex = 14;
+            this.TxtPass.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtPass.Location = new System.Drawing.Point(31, 301);
+            this.TxtPass.Name = "TxtPass";
+            this.TxtPass.Size = new System.Drawing.Size(211, 25);
+            this.TxtPass.TabIndex = 14;
             // 
-            // textBox1
+            // TxtKulAd
             // 
-            this.textBox1.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textBox1.Location = new System.Drawing.Point(11, 290);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(214, 25);
-            this.textBox1.TabIndex = 13;
+            this.TxtKulAd.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtKulAd.Location = new System.Drawing.Point(31, 228);
+            this.TxtKulAd.Name = "TxtKulAd";
+            this.TxtKulAd.Size = new System.Drawing.Size(214, 25);
+            this.TxtKulAd.TabIndex = 13;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.ForeColor = System.Drawing.Color.PaleGoldenrod;
-            this.label1.Location = new System.Drawing.Point(39, 262);
+            this.label1.Location = new System.Drawing.Point(59, 200);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(113, 18);
             this.label1.TabIndex = 12;
             this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(346, 166);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(334, 252);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
             // 
             // FrmAyarlar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSlateGray;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(665, 402);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Btnİslem);
             this.Controls.Add(this.simpleButton3);
             this.Controls.Add(this.simpleButton2);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TxtPass);
+            this.Controls.Add(this.TxtKulAd);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.gridControl1);
             this.Name = "FrmAyarlar";
@@ -149,6 +163,7 @@
             this.Load += new System.EventHandler(this.FrmAyarlar_Load);
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -158,12 +173,13 @@
 
         private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
         private DevExpress.XtraGrid.GridControl gridControl1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Btnİslem;
         private DevExpress.XtraEditors.SimpleButton simpleButton3;
         private DevExpress.XtraEditors.SimpleButton simpleButton2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TxtPass;
+        private System.Windows.Forms.TextBox TxtKulAd;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- ticari_otomasyon/FrmAyarlar.cs
+++ ticari_otomasyon/FrmAyarlar.cs
@@ -30,5 +30,16 @@ namespace ticari_otomasyon
         {
             listele();
         }
+
+        private void Btnİslem_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("insert into TBL_ADMIN values (@p1,@p2)", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", TxtKulAd.Text);
+            komut.Parameters.AddWithValue("@p2", TxtPass.Text);
+            komut.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            MessageBox.Show("Yeni Admin Sisteme Kaydedildi", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            listele();
+        }
     }
 }
